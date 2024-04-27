32fd043e0a5234f1e16f406c233ee667e3733138
0xMere <merenda.s.mattia@gmail.com>
2024-04-27 18:56:51 +0200

    Fixed indentation

--- src/ANN.cpp
+++ src/ANN.cpp
@@ -18,12 +18,12 @@ using namespace std;
  *                 the ANN topology.
  */
 ANN::ANN(const vector<unsigned> &topology) {
-  unsigned last = topology.size() - 1;
-  for (unsigned i = 0; i < last; ++i) {
-    layers.push_back(
-        Layer(i, topology.at(i), topology.at(i + 1))); // inner layer
-  }
-  layers.push_back(Layer(last, topology.at(last), 0)); // last layer
+	unsigned last = topology.size() - 1;
+	for (unsigned i = 0; i < last; ++i) {
+		layers.push_back(
+				Layer(i, topology.at(i), topology.at(i + 1))); // inner layer
+	}
+	layers.push_back(Layer(last, topology.at(last), 0)); // last layer
 }
 
 /**
@@ -34,13 +34,13 @@ ANN::ANN(const vector<unsigned> &topology) {
  * details of each layer.
  */
 void ANN::print() {
-  spdlog::info("Printing Artificial Neural Network");
+	spdlog::info("Printing Artificial Neural Network");
 
-  for (unsigned i = 0; i < layers.size(); ++i) {
-    layers[i].print();
-    cout << endl;
-  }
-  cout << endl;
+	for (unsigned i = 0; i < layers.size(); ++i) {
+		layers[i].print();
+		cout << endl;
+	}
+	cout << endl;
 }
 
 /**
@@ -53,41 +53,41 @@ void ANN::print() {
  * the DOT file.
  */
 void ANN::printCFG() {
-  std::string folderPath = "./graph";
-  std::string filename = folderPath + "/ann.dot";
-  namespace fs = std::__fs::filesystem;
+	std::string folderPath = "./graph";
+	std::string filename = folderPath + "/ann.dot";
+	namespace fs = std::__fs::filesystem;
 
-  if (!fs::exists(folderPath)) {
-    if (!fs::create_directory(folderPath)) {
-      spdlog::error("Unable to create directory {}", folderPath);
-      return;
-    }
-  }
+	if (!fs::exists(folderPath)) {
+		if (!fs::create_directory(folderPath)) {
+			spdlog::error("Unable to create directory {}", folderPath);
+			return;
+		}
+	}
 
-  std::ofstream outFile(filename);
+	std::ofstream outFile(filename);
 
-  if (!outFile.is_open()) {
-    spdlog::error("Unable to open {}", filename);
-    return;
-  }
+	if (!outFile.is_open()) {
+		spdlog::error("Unable to open {}", filename);
+		return;
+	}
 
-  outFile << "digraph unix {" << endl;
-  outFile << "rankdir=\"LR\";" << endl;
-  outFile << "ranksep=4;" << endl;
-  outFile << "nodesep=1.0;" << endl;
+	outFile << "digraph unix {" << endl;
+	outFile << "rankdir=\"LR\";" << endl;
+	outFile << "ranksep=4;" << endl;
+	outFile << "nodesep=1.0;" << endl;
 
-  for (unsigned i = 0; i < layers.size(); ++i)
-    layers[i].printSubgraphCFG(outFile, layers.size());
+	for (unsigned i = 0; i < layers.size(); ++i)
+		layers[i].printSubgraphCFG(outFile, layers.size());
 
-  for (unsigned i = 0; i < layers.size(); ++i)
-    layers[i].printCFG(outFile);
+	for (unsigned i = 0; i < layers.size(); ++i)
+		layers[i].printCFG(outFile);
 
-  outFile << "}" << endl;
-  outFile.close();
+	outFile << "}" << endl;
+	outFile.close();
 
-  spdlog::info("{} generated successfully.", filename);
-  spdlog::info("To generate PNG please use: ");
-  cout << "dot -Tpng " + filename + " -o " + folderPath + "/ann.png && open " +
-              folderPath + "/ann.png"
-       << endl;
+	spdlog::info("{} generated successfully.", filename);
+	spdlog::info("To generate PNG please use: ");
+	cout << "dot -Tpng " + filename + " -o " + folderPath + "/ann.png && open " +
+							folderPath + "/ann.png"
+			 << endl;
 }
