0ec39770f313ee54e2b2d47c2dec6e901d9b44ee
manueldiagostino <diagostinomanuel@gmail.com>
2024-05-04 17:47:30 +0200

    Commented virtual function `Trainer::backpropagate` not yet implemented ಠ_ಠ.

--- include/Trainer.h
+++ include/Trainer.h
@@ -7,46 +7,47 @@ namespace Training {
 
 /**
  * @brief A template class representing a trainer for neural networks.
- * 
+ *
  * @tparam Network_t The type of neural network used by the trainer.
  * @tparam Stream_t The type of data stream used by the trainer.
  * @tparam Tolerance_t The type representing the tolerance for convergence during training.
  * @tparam Step_t The type representing the step size for training.
  */
-template <typename Network_t = network_t, typename Stream_t = stream_t, typename Tolerance_t = tolerance_t, typename Step_t = step_t> 
+template <typename Network_t = network_t, typename Stream_t = stream_t,
+		  typename Tolerance_t = tolerance_t, typename Step_t = step_t>
 class Trainer {
   private:
-    // TODO fare documentazione
-    inline virtual std::vector<std::vector<ANN::data_t>>& backward_propagate(network_t& net, std::vector<std::vector<ANN::data_t>>& preactivation, 
-            const ANN::data_t& current_error, const step_t step) const = 0;
+	// TODO fare documentazione
+	/* inline virtual std::vector<std::vector<ANN::data_t>> & */
+	/* backward_propagate(network_t &net, std::vector<std::vector<ANN::data_t>> &preactivation, */
+	/* 				   const ANN::data_t &current_error, const step_t step) const = 0; */
 
-  public:    
-    using network_t = Network_t; /**< Type definition for the neural network type. */
-    using stream_t = Stream_t; /**< Type definition for the data stream type. */
-    using tolerance_t = Tolerance_t; /**< Type definition for the tolerance type. */
-    using step_t = Step_t; /**< Type definition for the step size type. */
+  public:
+	using network_t = Network_t;	 /**< Type definition for the neural network type. */
+	using stream_t = Stream_t;		 /**< Type definition for the data stream type. */
+	using tolerance_t = Tolerance_t; /**< Type definition for the tolerance type. */
+	using step_t = Step_t;			 /**< Type definition for the step size type. */
 
-    /**
-     * @brief Trains the neural network with the provided data stream.
-     * 
-     * @param net The neural network to train.
-     * @param stream The data stream used for training.
-     * @param tolerance The tolerance for convergence during training.
-     * @param epochs The maximum number of epochs for training.
-     * @param step The step size for training.
-     */
-    inline virtual void train(network_t& net, stream_t& stream, const tolerance_t tolerance, const size_t epochs, const step_t step) const = 0;
-    
-    /**
-     * @brief Tests the neural network with the provided data stream.
-     * 
-     * @param net The neural network to test.
-     * @param stream The data stream used for testing.
-     */
-    inline virtual void test(network_t& net, stream_t& stream) const = 0;
+	/**
+	 * @brief Trains the neural network with the provided data stream.
+	 *
+	 * @param net The neural network to train.
+	 * @param stream The data stream used for training.
+	 * @param tolerance The tolerance for convergence during training.
+	 * @param epochs The maximum number of epochs for training.
+	 * @param step The step size for training.
+	 */
+	inline virtual void train(network_t &net, stream_t &stream, const tolerance_t tolerance,
+							  const size_t epochs, const step_t step) const = 0;
+
+	/**
+	 * @brief Tests the neural network with the provided data stream.
+	 *
+	 * @param net The neural network to test.
+	 * @param stream The data stream used for testing.
+	 */
+	inline virtual void test(network_t &net, stream_t &stream) const = 0;
 };
 } // namespace Training
 
-
 #endif
-
